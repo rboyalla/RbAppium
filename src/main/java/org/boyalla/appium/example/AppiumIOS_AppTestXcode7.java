d39f13f6cc80095834e7184e09698863745f55e2
Rboyalla <rboyalla@gmail.com>
2021-03-20 22:43:29 -0700

    added .apk and .app files

--- src/main/java/org/boyalla/appium/example/AppiumIOS_AppTestXcode7.java
+++ src/main/java/org/boyalla/appium/example/AppiumIOS_AppTestXcode7.java
@@ -13,31 +13,39 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import java.io.File;
 
 public class AppiumIOS_AppTestXcode7 {
-	
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-            
-            File classpathRoot = new File(System.getProperty("user.home"));
-        File appDir = new File(classpathRoot, "rb/run/apps/");
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        //FileSystems.getDefault().getPath(".").toAbsolutePath();
+        //File appDir = new File(classpathRoot, "/Users/rb/rb/proj/RbAppium/apps");
+        File appDir = new File(classpathRoot, "apps");
+        //File app = new File(appDir, "TestAppRB.app");
+        //File app = new File("/Users/rb/rb/proj/RbAppium/apps/TestAppRB.app");
         File app = new File(appDir, "TestAppRB.app");
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.2");
-		//capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
-		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-		capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-		URL url = new URL("http://127.0.0.1:4723/wd/hub");
-		AppiumDriver<WebElement> driver = new AppiumDriver<WebElement>(url, capabilities);
-		
-		WebElement textBox=driver.findElement(By.name("txt1"));
-		textBox.sendKeys("raveendra");
-		Thread.sleep(10000);
-		WebElement button=driver.findElement(By.name("idb1"));
-		button.click();
-		Thread.sleep(1000);
-		driver.quit();
-		
-		//Thread.sleep(10000);
-		
-		System.out.println("DONE");
-		}
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
+        //capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+        //capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12 Pro Max");
+
+        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        AppiumDriver<WebElement> driver = new AppiumDriver<WebElement>(url, capabilities);
+
+        WebElement textBox = driver.findElement(By.name("User Name"));
+        System.out.println(" ######## before entering user name");
+        textBox.sendKeys("raveendra");
+        System.out.println(" ######## enetered user name");
+        Thread.sleep(10000);
+        WebElement button = driver.findElement(By.name("Set Default Label Text"));
+        System.out.println(" ######## enetered user name");
+        button.click();
+        Thread.sleep(1000);
+        driver.quit();
+
+        //Thread.sleep(10000);
+        System.out.println("DONE");
+    }
 }
